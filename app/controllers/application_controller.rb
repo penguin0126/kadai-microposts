4178fab9ba49081f8f41ece2f720ea840347e087
kubota <kimchinametake@gmail.com>
2018-03-15 15:23:54 +0900

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include SessionsHelper
-  
+
   private
 
   def require_user_loggind_in?
     unless logged_in?
       redirect_to login_url
     end
+  end
+
+  def count(user)
+    @count_micropost = user.microposts.count
   end
 end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,9 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @user = current_user
+      @micropost = current_user.microposts.build
+      @microposts =  current_user.posts.order('created_at DESD').page(params[:page])
+    end
   end
 end
